27a91fbbdbd1240194c2a2ba63dc4c488f6162fc
Yurii Budnyi <ybudnyi@gmail.com>
2021-11-10 16:19:45 +0200

    first commit

---
+++ jenkins/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y openjdk-11-jdk-headless
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo ufw allow 8080
+
